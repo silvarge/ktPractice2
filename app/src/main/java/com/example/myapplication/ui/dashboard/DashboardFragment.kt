12cfda53ecabb0f026a21ebfaa576eaa20f22d0e
silverage <sep.non2@gmail.com>
2024-07-29 01:07:27 +0900

    NumberPicker

--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.kt
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardFragment.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.NumberPicker
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
@@ -28,10 +29,30 @@ class DashboardFragment : Fragment() {
     _binding = FragmentDashboardBinding.inflate(inflater, container, false)
     val root: View = binding.root
     
+//     number picker 설정
+    dashboardViewModel.pickerValues.observe(viewLifecycleOwner) { values ->
+      binding.numberPicker.minValue = 0
+      binding.numberPicker.maxValue = values.size - 1
+      binding.numberPicker.displayedValues = values
+      binding.numberPicker.wrapSelectorWheel = true
+    }
+    
+//    선택된 인덱스 관찰
+    dashboardViewModel.selectedIndex.observe(viewLifecycleOwner) { index ->
+      binding.numberPicker.value = index
+    }
+    
+//    변경 이벤트 발생 시
+    binding.numberPicker.setOnValueChangedListener {_, _, newVal ->
+      dashboardViewModel.updateSelectedIndex(newVal)
+    }
+    
+    //  textView 설정
     val textView: TextView = binding.textDashboard
     dashboardViewModel.text.observe(viewLifecycleOwner) {
       textView.text = it
     }
+    
     return root
   }
   
--- app/src/main/java/com/example/myapplication/ui/dashboard/DashboardViewModel.kt
+++ app/src/main/java/com/example/myapplication/ui/dashboard/DashboardViewModel.kt
@@ -1,13 +1,36 @@
 package com.example.myapplication.ui.dashboard
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
 class DashboardViewModel : ViewModel() {
   
-  private val _text = MutableLiveData<String>().apply {
-    value = "This is dashboard Fragment"
+  //  선택 가능 값 설정
+  private val _pickerValues = MutableLiveData<Array<String>>().apply {
+    value = arrayOf("하나", "둘", "셋", "넷")
+  }
+  val pickerValues: LiveData<Array<String>> = _pickerValues
+  
+  //  현재 선택한 값의 인덱스 데이터
+  private val _selectedIndex = MutableLiveData<Int>().apply {
+    value = 0 // 기본값 (Default Index)
+  }
+  val selectedIndex: LiveData<Int> = _selectedIndex
+  
+  
+  private val _text = MediatorLiveData<String>().apply {
+    addSource(_selectedIndex) { index ->
+      value = _pickerValues.value?.get(index) ?: "선택된 값이 없습닏니다"
+    }
   }
   val text: LiveData<String> = _text
+  
+  //  인덱스 업데이트
+  fun updateSelectedIndex(index: Int) {
+    _selectedIndex.value = index
+  }
+  
 }
+
